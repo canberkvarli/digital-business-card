fbd1d221f64a60f15cacc4f44125e4013b9f30ef
Canberk Varli <canberkvarli@gmail.com>
2024-03-20 12:00:27 -0700

    add text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import "../stylesheets/about.css";
 
 export default function About() {
@@ -5,10 +6,24 @@ export default function About() {
     <>
       <div className="about-container">
         <h1>About</h1>
-        <p>Hello my name is Canberk.</p>
+        <p>
+          As a passionate software engineer, I am on a mission to turn
+          innovative ideas into exceptional web applications as well as testing
+          out the waters of mobile development and the blockchain concept. With
+          a solid background in Ruby, JavaScript, React/Redux, Python, Ruby on
+          Rails, Flask, PostgreSQL, Node.js, Solidity, Hardhat, Mongoose,
+          Express.js, and MongoDB, I thrive in the dynamic realm of technology.
+        </p>
 
         <h1>Interest</h1>
-        <p>These are my interests.</p>
+        <p>
+          Beyond the world of coding, you'll find me scaling rock walls, hitting
+          the pavement for a run, catching waves while surfing, and diving deep
+          into philosophical musings. I believe in the power of combining
+          creativity with technical expertise to craft digital solutions that
+          not only work seamlessly but also inspire. Let's connect and explore
+          the intersection of technology and creativity!`
+        </p>
       </div>
     </>
   );
