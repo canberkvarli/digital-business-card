1e45bb19fb5e615e64f55dfd76b08381a7f8e055
Canberk Varli <canberkvarli@gmail.com>
2024-03-20 12:05:45 -0700

    update bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,22 +7,16 @@ export default function About() {
       <div className="about-container">
         <h1>About</h1>
         <p>
-          As a passionate software engineer, I am on a mission to turn
-          innovative ideas into exceptional web applications as well as testing
-          out the waters of mobile development and the blockchain concept. With
-          a solid background in Ruby, JavaScript, React/Redux, Python, Ruby on
-          Rails, Flask, PostgreSQL, Node.js, Solidity, Hardhat, Mongoose,
-          Express.js, and MongoDB, I thrive in the dynamic realm of technology.
+          Passionate software engineer with expertise in full-stack web
+          development. Known for creativity and adaptability, I thrive in
+          dynamic tech environments, constantly seeking innovative solutions.
         </p>
 
         <h1>Interest</h1>
         <p>
-          Beyond the world of coding, you'll find me scaling rock walls, hitting
-          the pavement for a run, catching waves while surfing, and diving deep
-          into philosophical musings. I believe in the power of combining
-          creativity with technical expertise to craft digital solutions that
-          not only work seamlessly but also inspire. Let's connect and explore
-          the intersection of technology and creativity!`
+          Outside of coding, I'm scaling rock walls, running, surfing waves, and
+          delving into philosophical musings. I merge creativity with technical
+          expertise to craft inspiring digital solutions!
         </p>
       </div>
     </>
