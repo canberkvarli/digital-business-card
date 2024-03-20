4a4c204cf676c04508e6f0d0547343fadcf4643c
Canberk Varli <canberkvarli@gmail.com>
2024-03-20 11:04:27 -0700

    style

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -13,15 +13,15 @@ export default function Info() {
       <div className="buttons-container">
         <button style={{ backgroundColor: "#24292e", color: "#FFFFFF", border: "none", padding: "5px 10px", borderRadius: "5px" }}>
           <a href="https://github.com/canberkvarli" target="_blank" rel="noopener noreferrer" style={{ color: "#FFFFFF", textDecoration: "none" }}>
-            <FaGithub size={20} style={{ marginRight: "10px"}} />
-            <span>Github</span>
+            <FaGithub size={15} style={{ marginRight: "5px", paddingTop: "5px"}} />
+            <span style={{marginBottom: "5px"}}>Github</span>
           </a>
         </button>
 
         <button style={{ backgroundColor: "#0077b5", color: "#FFFFFF", border: "none", padding: "5px 10px", borderRadius: "5px" }}>
           <a href="https://www.linkedin.com/in/canberkvarli/" target="_blank" rel="noopener noreferrer" style={{ color: "#FFFFFF", textDecoration: "none" }}>
-            <FaLinkedin size={20} style={{ marginRight: "10px" }} />
-            <span>LinkedIn</span>
+            <FaLinkedin size={15} style={{ marginRight: "5px", paddingTop: "5px"}} />
+            <span style={{margin: "10px;"}}>LinkedIn</span>
           </a>
         </button>
       </div>
